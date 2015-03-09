22079b6aef15e436c879dda0120fc26a95dc18c4
Niall O'Brien <niall.obrien@clock.co.uk>
2015-03-09 02:08:53 +0000

    > Creating posts

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,5 +1,5 @@
 Template.postItem.helpers({
-    domain: function() {
+    domain: function () {
         var a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
